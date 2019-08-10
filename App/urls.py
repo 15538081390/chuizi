5ed032544230b0af684902fd6058acdaebc9338b
lichao <1369605126@qq.com>
2019-08-10 17:32:15 +0800

    cc

--- App/urls.py
+++ App/urls.py
@@ -6,6 +6,8 @@ urlpatterns =[
 
     url(r"^index/$",views.index,name="index"),
     url(r"^second/(\d+)/$",views.second,name="second"),
-
+    url(r"^server/$",views.server,name="server"),
+    url(r"^server2/$",views.server2,name="server2"),
+    url(r"^server3/$",views.server3,name="server3")
 
 ]
--- App/views.py
+++ App/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from App.models import *
 
 # Create your views here.
-
+#首页
 def index(request):
     home=Indexhome.objects.all()
     tab=IndexTab.objects.all()
@@ -15,22 +15,39 @@ def index(request):
 
     # return HttpResponse("111111")
 
-
+# 商品展示
 def second(request,cid):
     home = Indexhome.objects.all()
     tab = IndexTab.objects.all()
     copy=Indexcopy.objects.all()
     indepro=Indexproduct.objects.all()
     products=Productcategorie.objects.all()
-
-
-
     return render(request,"App/bash/second.html",context={"home":home,
                                                         "tab":tab,
                                                         "products":products,
                                                           "to_id":int(cid),
                                                           "copy":copy,
                                                           "indepro":indepro,})
+
+
+
+#服务条款
+def server(request):
+    tab=IndexTab.objects.all()
+    return render(request,"App/bash/server.html",context={"tab":tab,
+                                                          })
+#售后政策
+def server2(request):
+    tab=IndexTab.objects.all()
+    return render(request,"App/bash/server2.html",context={"tab":tab,
+                                                          })
+#保修服务
+def server3(request):
+    tab=IndexTab.objects.all()
+    home=Indexhome.objects.all()
+    return render(request,"App/bash/server3.html",context={"tab":tab,
+                                                           "home":home,
+                                                          })
 
 
 
